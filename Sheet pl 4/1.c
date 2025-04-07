8c84f69608ee37e8511c29c56b4d5a39bdb5c3d7
Mr.Shadow <97762724+Shadowlo21@users.noreply.github.com>
2025-04-07 11:31:57 +0200

    Update 1.c

--- Sheet pl 4/1.c
+++ Sheet pl 4/1.c
@@ -1,25 +1,24 @@
-
 #include <stdio.h>
 #define SIZE 10
 int main() {
     int numbers[SIZE];
     int* ptr = numbers;
-    int  max, min;
-    float sum = 0;
     printf("Enter %d numbers: \n", SIZE);
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++)
         scanf("%d", (ptr + i));
-    }
-    max = min = 0;
+    int* max = ptr;
+    int* min = ptr;
+    float sum = 0;
     for (int i = 0; i < SIZE; i++) {
-        if (max < *(ptr + i)) {
-            max = *(ptr + i);
+        if (*max < *(ptr + i)) {
+            max = (ptr + i);
         }
-        if (min  > *(ptr + i)) {
-            min = *(ptr + i);
+        if (*min > *(ptr + i)) {
+            min = (ptr + i);
         }
         sum += *(ptr + i);
     }
-    printf("Maximum: %d\nMinimum: %d\nAverage: %.2f\n", max,min, sum / SIZE);
+
+    printf("Maximum: %d\nMinimum: %d\nAverage: %.2f\n", *max, *min, sum / SIZE);
     return 0;
 }
